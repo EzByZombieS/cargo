1c26cec0879d3c45b992aed6540f132f8c4d9834
Samuel Jefri Saputra Siahaan <91043243+EzByZombieS@users.noreply.github.com>
2022-06-10 19:52:50 +0700

    update user

--- resources/views/pages/user/dashboard/main.blade.php
+++ resources/views/pages/user/dashboard/main.blade.php
@@ -1,3 +1,37 @@
+@section('css')
+<style>
+.calendar-toolbar {
+  margin-bottom: 10px;
+}
+.calendar-month-row {
+  height: 75px;
+}
+.calendar-prior-months-date {
+  color: #DDD;
+}
+.calendar-current-date {
+  text-align: center;
+  display: inline-block;
+  width: 115px;
+}
+.contain{
+  padding: 5% 10%;
+  font-family: "Poppins", sans-serif;
+}
+
+.time {
+  font-size: 5rem;
+  display: inline-block;
+}
+
+.time-per {
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  margin-left: 1em;
+}
+</style>
+@endsection
 <x-web-layout title="Dashboard">
 
     <div class="row g-5 g-xl-8">
@@ -18,6 +52,60 @@
                                 <span class="text-gray-800 text-hover-primary fs-6">Jangan Berikan Username dan Password Anda pada Siapapun</span> <br>
                             </div>
                             <span class="fw-bolder text-info py-1"><img alt="Logo" src="{{ asset('img/logo.png') }}" class="h-200px" /><br></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body p-lg-17">
+            <div class="row g-5 mb-5 mb-lg-15">
+                <div class="col-sm-6 pe-lg-10">
+                    <div class="text-center bg-light card-rounded d-flex flex-column justify-content-center p-10 h-100">
+                        <div class="container">
+
+                            <h1 class="page-header">
+                              <i class="fa fa-calendar"></i> Kalendar
+                            </h1>
+                            
+                            <div class="calendar" id="calendar">
+
+                              <div class="calendar-toolbar">
+
+                                <button data-toggle="calendar" data-action="today" class="btn btn-default">
+                                  Today
+                                </button>
+
+                                <button data-toggle="calendar" data-action="prev" class="btn btn-default">
+                                  <i class="fa fa-chevron-left"></i>
+                                </button>
+
+                                <div class="calendar-current-date "
+                                      data-day-format="MM/DD/YYYY"
+                                      data-week-format="MM/DD/YYYY"
+                                      data-month-format="MMMM, YYYY">
+                                  (placeholder)
+                                </div>
+
+                                <button data-toggle="calendar" data-action="next" class="btn btn-default">
+                                  <i class="fa fa-chevron-right"></i>
+                                </button>
+                                
+                              </div>                          
+                            </div>
+                          </div>                           
+                    </div>
+                </div>
+                <div class="col-sm-6 ps-lg-10">
+                    <div class="text-center bg-light card-rounded d-flex flex-column justify-content-center p-10 h-100">
+                        <div class="container contain">
+                            <h1 class="page-header">
+                                <i class="fa fa-calendar"></i> Waktu
+                            </h1>
+                            <div class="time">12:05:32</div>
+                            <div class="time-per">am</div>
                         </div>
                     </div>
                 </div>
